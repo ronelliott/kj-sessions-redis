18cde25c44219eca77f3fe73c8d27155ea34db81
Ron Elliott <ronaldbelliott@gmail.com>
2016-04-13 09:20:25 -0700

    Converted to ES6.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var extend = require('extend'),
-    is = require('is'),
-    session = require('express-session'),
-    RedisStore = require('connect-redis')(session);
+const is = require('is'),
+      session = require('express-session'),
+      RedisStore = require('connect-redis')(session);
 
 module.exports = function($opts) {
     return function($$app, $$resolver) {
         if ($opts.enabled) {
             var client = is.string($opts.resource) ? $$resolver($opts.resource) : $opts.resource;
-            $$app.use(session(extend(true, {
+            $$app.use(session(Object.assign({
                 store: new RedisStore({ client: client })
             }, $opts.options)));
         }
